fd3a8845c9f4ff271aa750b684d2d08e93dd589c
SiwenX <42300606+SiwenX@users.noreply.github.com>
2018-11-06 09:22:18 -0500

    Update GATK_PON.sh

--- GATK_PON.sh
+++ GATK_PON.sh
@@ -6,9 +6,18 @@
 #PBS -M siwxu@iu.edu
 #PBS -m abe
 
-/gpfs/hps/soft/rhel7/gatk/4.0.4.0/gatk Mutect2 \
--R /gpfs/home/s/i/siwxu/Carbonate/database/hg38.fa \
--I /N/dc2/projects/ngs/users/siwxu/projects/myeloma/3/3_normal.sort.bam \
--tumor 3_normal \
---disable-read-filter MateOnSameContigOrNoMappedMateReadFilter \
--O /N/dc2/projects/ngs/users/siwxu/projects/myeloma/3/3_PON.vcf.gz
+##Create PON
+#/gpfs/hps/soft/rhel7/gatk/4.0.4.0/gatk Mutect2 \
+#-R /gpfs/home/s/i/siwxu/Carbonate/database/hg38.fa \
+#-I /N/dc2/projects/ngs/users/siwxu/projects/myeloma/3/3_normal.sort.bam \
+#-tumor 3_normal \
+#--disable-read-filter MateOnSameContigOrNoMappedMateReadFilter \
+#-O /N/dc2/projects/ngs/users/siwxu/projects/myeloma/3/3_PON.vcf.gz
+##Collate all the normal VCFs into a single callset
+/gpfs/hps/soft/rhel7/gatk/4.0.4.0/gatk CreateSomaticPanelOfNormals \
+-vcfs /N/dc2/projects/ngs/users/siwxu/projects/myeloma/2/2_PON.vcf.gz \
+-vcfs /N/dc2/projects/ngs/users/siwxu/projects/myeloma/3/3_PON.vcf.gz \
+-vcfs /N/dc2/projects/ngs/users/siwxu/projects/myeloma/4/4_PON.vcf.gz \
+-vcfs /N/dc2/projects/ngs/users/siwxu/projects/myeloma/5/5_PON.vcf.gz \
+-vcfs /N/dc2/projects/ngs/users/siwxu/projects/myeloma/8/8_PON.vcf.gz \
+-O /N/dc2/projects/ngs/users/siwxu/projects/myeloma/Fivesamplepon.vcf.gz
